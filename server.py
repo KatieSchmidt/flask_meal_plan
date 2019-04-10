996abc68986e1e56394d43aedd7cd1a3250e6169
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-10 15:22:58 -0400

    add get_meal_by_id but it doesnt handle errors yet....

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-from flask import request
+from flask import request, jsonify
 from flask import Flask
 from bson.json_util import dumps
-import bson
+from bson.objectid import ObjectId
 import datetime
 import pprint
 
-DATABASE = MongoClient('localhost', 27017)["meal_plan"]
+DATABASE = MongoClient('localhost', 27017)["go_meals"]
 
 meals = DATABASE.meals
 
@@ -17,7 +17,7 @@ app = Flask(__name__)
 @app.route('/meals', methods=["POST"])
 def create_meal():
     new_meal = {
-        "ID": bson.objectid.ObjectId(),
+        "ID": ObjectId(),
         "username": request.form.get("username"),
         "mealname": request.form.get('mealname'),
         "totalcalories": 0,
@@ -30,9 +30,15 @@ def create_meal():
 
 @app.route('/meals', methods=["GET"])
 def get_meals():
-    errors = {}
     all_meals = meals.find()
     if all_meals.count() > 0:
         return dumps(meals.find())
     else:
         return "Error: No meals found", 404
+
+
+@app.route('/meals/<meal_id>/', methods=["GET"])
+def get_meal_by_id(meal_id):
+    print(meal_id)
+    meal = meals.find_one({"_id": ObjectId(meal_id)})
+    return dumps(meal)
