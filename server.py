b8c7c9afc346d57af2da2cc4f611c6fe80572b8e
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-10 18:15:14 -0400

    create add_ingredient_to_meal route

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import request, jsonify
+from flask import request
 from flask import Flask
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -9,6 +9,7 @@ import pprint
 DATABASE = MongoClient('localhost', 27017)["go_meals"]
 
 meals = DATABASE.meals
+mealplans = DATABASE.mealplans
 
 # flask config
 app = Flask(__name__)
@@ -20,8 +21,8 @@ def create_meal():
         "ID": ObjectId(),
         "username": request.form.get("username"),
         "mealname": request.form.get('mealname'),
-        "totalcalories": 0,
-        "ingredients": [],
+        "totalcalories": float(0),
+        "ingredients": list(),
         "dateadded": datetime.datetime.now()
     }
     meal_id = meals.insert_one(new_meal).inserted_id
@@ -41,4 +42,27 @@ def get_meals():
 def get_meal_by_id(meal_id):
     print(meal_id)
     meal = meals.find_one({"_id": ObjectId(meal_id)})
+    return dumps(meal)
+
+
+@app.route('/meals/<meal_id>/', methods=["POST"])
+def add_ingredient_to_meal(meal_id):
+    print(meal_id)
+    filter = {"_id": ObjectId(meal_id)}
+    meal = meals.find_one(filter)
+    ingredient = {
+        "id": ObjectId(),
+        "ingredient": request.form.get("ingredient"),
+        "totalcalories": request.form.get("calories"),
+        "measureunit": request.form.get("measureunit"),
+        "measureunitquantity": request.form.get("measureunitquantity")
+    }
+    print(meal["ingredients"])
+    print(meal["totalcalories"])
+    print(ingredient["totalcalories"])
+    print(type(meal["ingredients"]))
+    meal["ingredients"].append(ingredient)
+    meal["totalcalories"] += float(ingredient["totalcalories"])
+
+    meals.replace_one(filter, meal)
     return dumps(meal)
